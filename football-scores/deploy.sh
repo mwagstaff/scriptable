e33e85471af39bf3b01381321407b31f089e300c
Mike Wagstaff <mike.wagstaff@gmail.com>
2024-06-21 23:19:33 +0100

    tweak copy command

--- football-scores/deploy.sh
+++ football-scores/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Copy FootballScores.js to the Scriptable directory
-cp FootballScores.js ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents/FootballScores.js
+cp FootballScores.js ~/Library/Mobile\ Documents/iCloud~dk~simonbs~Scriptable/Documents
